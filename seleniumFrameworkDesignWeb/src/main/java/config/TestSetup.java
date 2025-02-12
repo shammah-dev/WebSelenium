100f5d33cebb2e0ecda50a8f14e33477474b9909
shammah-dev <shammah3110@gmail.com>
2025-02-11 19:20:27 -0500

    Parallel execution using Test Grid update

--- seleniumFrameworkDesignWeb/src/main/java/config/TestSetup.java
+++ seleniumFrameworkDesignWeb/src/main/java/config/TestSetup.java
@@ -1,14 +1,18 @@
 package config;
 
+import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -20,16 +24,22 @@ public class TestSetup {
 		 
 	
 	@BeforeClass
-	public void beforeClass() throws Exception {
-		driver.set(OpenBrowser());
+	@Parameters({"browserName"})
+	public void beforeClass(String browserName) throws Exception {
+		if(Utilities.getProperty("execution").equalsIgnoreCase("remote")) {
+			driver.set(OpenBrowser(browserName));	
+		}else {
+		browserName= Utilities.getProperty("browserName");
+		driver.set(OpenBrowser(browserName));
+		}
 	}
 	
 	 public static WebDriver getDriver() {
 			return driver.get();
 		}
 	
-	public static WebDriver OpenBrowser() throws Exception {
-	String BrowserName= Utilities.getProperty("browserName");
+	public static WebDriver OpenBrowser(String BrowserName) throws Exception {
+	DesiredCapabilities caps = new DesiredCapabilities();
 	WebDriver localDriver=null;
 	switch(BrowserName.toLowerCase()) {
 	case "chrome":
@@ -49,6 +59,18 @@ public class TestSetup {
 		localDriver =new  EdgeDriver();
 		localDriver.manage().window().maximize();
 		localDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	case "remotechrome":
+		caps.setBrowserName("chrome");
+		localDriver = new RemoteWebDriver(new URL("http://10.0.0.100:4444"),caps);
+		localDriver.manage().window().maximize();
+		localDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		break;
+	case "remotefirefox":
+		caps.setBrowserName("firefox");
+		localDriver = new RemoteWebDriver(new URL("http://10.0.0.100:4444"),caps);
+		localDriver.manage().window().maximize();
+		localDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		break;
 	case "default":
 		throw new Exception("Browser not configured.");
 
